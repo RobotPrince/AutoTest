b5dd77bf34b3eae11f2605f84be789ca3cbf25c9
os-tll <921965253@qq.com>
2018-10-16 17:47:09 +0800

    tll chrome

--- WebDriverTest/src/main/java/com/yss/common/Common.java
+++ WebDriverTest/src/main/java/com/yss/common/Common.java
@@ -67,12 +67,12 @@ public class Common {
 	//	System.setProperty("webdriver.chrome.bin", "/opt/google/chrome/chrome");
 		ChromeOptions options = new ChromeOptions();
 		
-		options.addArguments("--headless");
-		options.addArguments("--disable-extensions"); // disabling extensions
-		options.addArguments("--disable-gpu"); // applicable to windows os only
-		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-		options.addArguments("--no-sandbox"); // Bypass OS security model
-		options.addArguments("--disable-infobars"); // disabling infobars
+		options.addArguments("headless");
+		options.addArguments("disable-extensions"); // disabling extensions
+		options.addArguments("disable-gpu"); // applicable to windows os only
+		options.addArguments("disable-dev-shm-usage"); // overcome limited resource problems
+		options.addArguments("no-sandbox"); // Bypass OS security model
+		options.addArguments("disable-infobars"); // disabling infobars
 		System.out.println("before driver");
 		WebDriver driver = new ChromeDriver(options);
 		driver.get("http://www.baidu.com");
