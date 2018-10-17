1077862971a2a50f53fca7f6e956370f5470ddc6
os-tll <921965253@qq.com>
2018-10-17 15:54:53 +0800

    commit para

--- WebDriverTest/src/main/java/com/yss/suit/scenario1.java
+++ WebDriverTest/src/main/java/com/yss/suit/scenario1.java
@@ -1,10 +1,12 @@
 package com.yss.suit;
 
 import org.apache.log4j.Logger;
+import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.yss.common.Common;
@@ -25,10 +27,18 @@ public class scenario1 {
 	 * 如：1.登陆 2.开户 3.申购
 	 */
 	@Test(priority = 0)
-	public void login() {
-
-		new Login().login();
-		Reporter.log("登录成功");
+	@Parameters({"TA_address","user","password"})
+	public void login(String TA_address, String user, String password) {
+		try{
+		
+			if(!new Login().login(TA_address, user, password)){
+				Reporter.log("登录失败");
+				Assert.fail("登录失败");
+			}
+		}catch(Exception e){
+			Reporter.log("登录失败");
+			Assert.fail("登录失败");
+		}
 	}
 	
 	@Test(priority = 1)
