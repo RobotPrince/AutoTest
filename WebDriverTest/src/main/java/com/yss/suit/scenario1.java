776ace85d6f0982c6e0bdc216ab40f08bcb44d3e
os-tll <921965253@qq.com>
2018-10-22 14:37:27 +0800

    commit tll

--- WebDriverTest/src/main/java/com/yss/suit/scenario1.java
+++ WebDriverTest/src/main/java/com/yss/suit/scenario1.java
@@ -135,23 +135,23 @@ public class scenario1 {
 //		Reporter.log("产品费率-审核成功");
 //	}
 	
-	@Test(priority = 16)
-	public void ChanPinXiaoShouDaiLiGuanXiAdd() throws InterruptedException{
-		new ChanPinXiaoShouDaiLiGuanXi().before();
-		new ChanPinXiaoShouDaiLiGuanXi().add();
-		Reporter.log("产品销售代理关系-新增成功");
-	}
-	@Test(priority = 17)
-	public void ChanPinXiaoShouDaiLiGuanXiView() throws InterruptedException{
-		new ChanPinXiaoShouDaiLiGuanXi().view();
-		Reporter.log("产品销售代理关系-查看成功");
-	}
-	@Test(priority = 18)
-	public void ChanPinXiaoShouDaiLiGuanXiReview() throws InterruptedException{
-		new ChanPinXiaoShouDaiLiGuanXi().review();
-		new ChanPinXiaoShouDaiLiGuanXi().after();
-		Reporter.log("产品销售代理关系-审核成功");
-	}
+//	@Test(priority = 16)
+//	public void ChanPinXiaoShouDaiLiGuanXiAdd() throws InterruptedException{
+//		new ChanPinXiaoShouDaiLiGuanXi().before();
+//		new ChanPinXiaoShouDaiLiGuanXi().add();
+//		Reporter.log("产品销售代理关系-新增成功");
+//	}
+//	@Test(priority = 17)
+//	public void ChanPinXiaoShouDaiLiGuanXiView() throws InterruptedException{
+//		new ChanPinXiaoShouDaiLiGuanXi().view();
+//		Reporter.log("产品销售代理关系-查看成功");
+//	}
+//	@Test(priority = 18)
+//	public void ChanPinXiaoShouDaiLiGuanXiReview() throws InterruptedException{
+//		new ChanPinXiaoShouDaiLiGuanXi().review();
+//		new ChanPinXiaoShouDaiLiGuanXi().after();
+//		Reporter.log("产品销售代理关系-审核成功");
+//	}
 //	@Test(priority = 19)
 //	public void FeiYongFenChengAdd() throws InterruptedException{
 //		new FeiYongFenCheng().before();
