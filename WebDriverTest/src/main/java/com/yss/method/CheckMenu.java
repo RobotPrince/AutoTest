14721afbe0e378efde35a06af493c4f20c85a0de
bianqigang <bianqigang@ysstech.com>
2018-11-01 15:21:48 +0800

    commit  违约赎回费率  second

--- WebDriverTest/src/main/java/com/yss/method/CheckMenu.java
+++ WebDriverTest/src/main/java/com/yss/method/CheckMenu.java
@@ -111,6 +111,7 @@ public class CheckMenu {
 		/**
 		 * 固定收益利率
 		 */
+		GUDINGSHOUYILILV_2,
 		GUDINGSHOUYILILU_2,
 		/**
 		 * 业绩报酬方案
@@ -169,10 +170,6 @@ public class CheckMenu {
 		 */
 		ZHEKOUGUANLI_2,
 		/**
-		 * 执行权限设置
-		 */
-		ZHIXINGQUANXIANSHEZHI_1,
-		/**
 		 * 执行权限人信息
 		 */
 		ZHIXINGQUANXIANRENXINXI_2,
@@ -188,7 +185,7 @@ public class CheckMenu {
 		 * 产品清算周期
 		 */
 		CHANPINQINGSUANZHOUQI_2,
-		/*
+		/**
 		 * 执行权限设置
 		 */
 		CHANPINQUANXIANSHEZHI_1,
